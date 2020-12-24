a91da4c27b674f5d158723edc07a9627ff8d0536
Jono <jono@roboto.studio>
2020-12-24 11:55:29 +0000

    Fixed the little tricky bits

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `tripathi-christmas-2020`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#CC231E`,
+        theme_color: `#CC231E`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tree.png`, // This path is relative to the root of the site.
       },
     },
     {
